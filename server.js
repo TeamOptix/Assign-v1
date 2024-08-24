dd11a29013b04f4ae6b6528df6d9564d2c9936ea
dotNulled <20d.dhruv@gmail.com>
2024-08-24 23:02:38 +0530

    Switch to express.js, implement basic json database functionality

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('static'));
+
+let day;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/templates/home.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/templates/login.html');
+});
+
+app.get('/teachers/:name', (req, res) => {
+    res.send(req.params);
+    console.log(req.params.name);
+
+    let loc_def = __dirname + '/database/default/' + req.params.name + '/tt.json';
+    const tt_def = JSON.parse(fs.readFileSync(loc_def));
+    console.log(tt_def);
+});
+
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    res.send(`Username: ${username} Password: ${password}`);
+});
+
+const port = 3000
+app.listen(port, () => console.log(`This app is listening on port ${port}`));
